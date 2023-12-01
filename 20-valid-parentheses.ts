f439d684e963833b743d526dd2f47583dc73c890
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-01 09:46:56 -0800

    completed all current challenges

--- 20-valid-parentheses.ts
+++ 20-valid-parentheses.ts
@@ -1,29 +1,23 @@
 // function isValid(s: string): boolean {
 function isValid(s) {
-  if (s.indexOf("(") != -1) {
-    if (s[s.indexOf("(") + 1] != ")") {
-      return false;
-    }
+  let stack = [];
+  let valid = ["()", "[]", "{}"];
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s[i]);
+    let compare = (
+      stack[stack.length - 2] + stack[stack.length - 1]
+    ).toString();
+    valid.includes(compare) && stack.splice(stack.length - 2, 2);
   }
-  if (s.indexOf("([") != -1) {
-    if (s[s.indexOf("[") + 1] != "]") {
-      return false;
-    }
-  }
-  if (s.indexOf("{") != -1) {
-    if (s[s.indexOf("{") + 1] != "}") {
-      return false;
-    }
-  }
-  return true;
+  return stack.length > 0 ? false : true;
 }
 
 let one = { s: "()" };
 let two = { s: "()[]{}" };
 let three = { s: "(]" };
 
-console.log(isValid(one.s));
-console.log(isValid(two.s));
-console.log(isValid(three.s));
+// console.log(isValid(one.s));
+// console.log(isValid(two.s));
+// console.log(isValid(three.s));
 
 // node 20-valid-parentheses.ts
